4ae0ee6335ed7a00e7968184a0cc20e69cb6e933
mbp.user <mbp@git.upall.cn>
2020-08-02 09:36:52 +0800

    add evaluate function

--- config.fish
+++ config.fish
@@ -41,10 +41,35 @@ alias vs="cd ~/Homestead/ && vagrant up && vagrant ssh && cd -"
 alias vh="cd ~/Homestead/ && vagrant halt && cd -"
 alias vr="cd ~/Homestead/ && vagrant reload --provision && cd -"
 
-alias resetUBar="rm -f ~/Library/Preferences/ca.brawer.uBar.plist"
-alias resetAF="rm -rf ~/Library/Group\ Containers/6LVTQB9699.com.seriflabs && rm -f ~/Library/Preferences/com.seriflabs.affinityphoto.plist"
-
-alias fixDock="sudo killall launchservicesd && sudo killall Dock"
-
 set -gx PATH $HOME/.bin $PATH
+
+function evaluate --description 'Reset App Evaluation'
+  if test (count $argv) -lt 1
+    echo 'Reset App Evaluation'
+    echo 'Usage: '$_' AffinityPhoto|PhpStorm|UBar|Dock'
+    return
+  end
+  set app (string lower $argv[1])
+  switch $app
+    case 'affinityphoto'
+      killall 'Affinity Photo'
+      rm -rf ~/Library/Group\ Containers/6LVTQB9699.com.seriflabs
+      rm -f ~/Library/Preferences/com.seriflabs.affinityphoto.plist
+      return
+    case 'phpstorm'
+      killall phpstorm
+      rm -rf ~/Library/Application\ Support/JetBrains/PhpStorm*/eval
+      sed -i /name=\"evl/d ~/Library/Application\ Support/JetBrains/PhpStorm*/options/other.xml
+      return
+    case 'ubar'
+      rm -f ~/Library/Preferences/ca.brawer.uBar.plist
+      return
+    case 'dock'
+      sudo killall launchservicesd
+      sudo killall Dock
+      return
+    case '*'
+      echo 'Unknown App'
+  end
+end
 
